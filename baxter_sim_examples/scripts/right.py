0b3d7fe72f36fb400dc7781125101e56bdfdece4
GiuseppeCannata <thebloodofjazz29@gmail.com>
2019-03-07 14:10:08 +0100

    Ricommento right.py

--- baxter_sim_examples/scripts/right.py
+++ baxter_sim_examples/scripts/right.py
@@ -9,9 +9,9 @@ import moveit_msgs.msg
 
 # Messaggi
 from geometry_msgs.msg import (
-    Pose,
-    Point,
-    Quaternion,
+    Pose,        # http://docs.ros.org/lunar/api/geometry_msgs/html/msg/Pose.html
+    Point,       # http://docs.ros.org/lunar/api/geometry_msgs/html/msg/Point.html
+    Quaternion,  # http://docs.ros.org/lunar/api/geometry_msgs/html/msg/Quaternion.html
 )
 
 from std_msgs.msg import Bool
@@ -26,22 +26,26 @@ from moduloMessaggi import (
 
 def right():
 
-    rospy.init_node("right_arm")
-    r_braccio = Braccio("right")
+    rospy.init_node("right_arm")  # Iniziallizziamo il nodo ROS
+    r_braccio = Braccio("right")  # Istanziamo la classe Baxter passandogli il nome
 
     # Attende che il sinistro abbia trovato il punto di scambio
     data = rospy.wait_for_message("left/msg", Pose, timeout=None)
     r_braccio.gripper_open()
-    # Punto di partenza 1
+	
+    # PRIMO PUNTO DI ARRIVO
     start1 = Pose( position= Point(x=0.0511116965597, y=-0.864422194945, z=-0.305731378478),
                  orientation= Quaternion(x=0.471615536924, y=0.881779971596, z=-0.00562827161274, w=-0.00334508327601))
     r_braccio.move_with_pose(start1)
     rospy.sleep(1)
-    # Punto di partenza 2
+	
+    # SECONDO PUNTO DI ARRIVO
     start2 = Pose( position= Point(x=0.469330967229, y=-0.55023988915, z=0.404403189229),
                  orientation= Quaternion(x=0.471577579986, y=0.881800825784, z=-0.00558788488991, w=-0.00326576857884))
     r_braccio.move_with_pose(start2)
     rospy.sleep(1)
+	
+	# TERZO PUNTO DI ARRIVO
     starting_joint_angles = {'right_w0': 1.4337025470874254,
                              'right_w1': 1.6612876079132552,
                              'right_w2': -1.4625539596386004,
@@ -51,30 +55,38 @@ def right():
                              'right_s1': -0.3642896913230187}
     r_braccio.move_with_joints(starting_joint_angles)
     
+	# Spostiamo il braccio nella posizione indicata dal braccio sinistro
     data.position = Point(x= 0.585560666885, y= 0.209368041126, z= 0.105)
     data.orientation = Quaternion(x=-0.01508949187, y= 0.705381077844, z= 0.707937222017, w= 0.032167249461)
     rospy.sleep(1)
     r_braccio.move_with_pose(data)
-    #r_braccio.prova(data)
-
     rospy.sleep(1)
-    r_braccio.gripper_close()
+	
+	# Afferriamo il pezzo
+    r_braccio.gripper_close() 
 
+	# Diciamo al braccio sinistro di aver preso il pezzo
     talker("right/msg", Bool, 1, True)
+	
+	# Attendiamo che il braccio sinistro abbia aperto il gripper
+	# data == True il gripper è aperto
     data = rospy.wait_for_message("left1/msg", Bool, timeout=None)
 
     if data:
+	
        # Punto intermedio 1 (sotto il gripper sx)
        int1 = Pose( position= Point(x=0.59646481601, y=0.256599564965, z=-0.0152786373748),
                  orientation= Quaternion(x=-0.00553449776039, y=0.698224446563, z=0.712501515165, w=0.0692357020905))
        r_braccio.move_with_pose(int1)
        rospy.sleep(1)
+	   
        # Punto intermedio 2 
        int2 = Pose( position= Point(x=0.597068370528, y=-0.0457122506092, z=0.00175406270759),
                  orientation= Quaternion(x=-0.00562289135545, y=0.698260045752, z=0.712471697084, w=0.0691763865266))
        r_braccio.move_with_pose(int2)
        rospy.sleep(1)
 
+	   # QUARTO PUNTO DI ARRIVO
        final_joint_angles = {'right_w0': 1.4536034401562068,
                              'right_w1': 1.3508498030339906,
                              'right_w2': -1.380729984871202,
