403eb068f5788f7e98e60ab89dbcb70d816a96fb
GiuseppeCannata <thebloodofjazz29@gmail.com>
2019-03-07 14:24:38 +0100

    Ricommento modulooMessaggi.py

--- baxter_sim_examples/scripts/moduloMessaggi.py
+++ baxter_sim_examples/scripts/moduloMessaggi.py
@@ -11,6 +11,14 @@ from geometry_msgs.msg import (
 from std_msgs.msg import Bool
 import baxter_interface
 
+"""
+Permette di scrivere un messaggio sul topic specificato
+
+:param name_topic: nome del topic sul quale si vuole scrivere
+:param tipo_messaggio: tipo del messaggio che scriviamo sul topic
+:param size_coda: lunghezza del buffer del topic
+:param msg: messaggio 
+"""
 def talker(name_topic, tipo_messaggio, size_coda, msg):
 
        pub = rospy.Publisher(name_topic, tipo_messaggio, queue_size=size_coda)
@@ -20,8 +28,15 @@ def talker(name_topic, tipo_messaggio, size_coda, msg):
 
        rospy.sleep(1)
        pub.publish(msg)
-       rospy.loginfo("Ho pubblicato")
+       rospy.loginfo("Ho pubblicato
+	   
         
+"""
+Permette di scrivere un messaggio sul topic specificato
 
+:param name_topic: nome del topic da cui vogliamo leggere
+:param tipo_messaggio: tipo del messaggio che vogliamo leggere
+:param function: funzione da applicare al messaggio che leggiamo
+"""
 def listener(name_topic, tipo_messaggio, function):
        rospy.Subscriber(name_topic, tipo_messaggio, function) 
